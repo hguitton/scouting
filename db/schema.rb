eb7bdb1a1a30c55f4c831042665ff3803ad3b6fc
Hugo Guitton <guitton.h@gmail.com>
2020-03-28 21:18:59 +0100

    Postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_141512) do
+ActiveRecord::Schema.define(version: 2020_03_28_145029) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "player_id"
+    t.integer "user_id"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["player_id"], name: "index_comments_on_player_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "players", force: :cascade do |t|
     t.string "lastname"
@@ -29,6 +42,41 @@ ActiveRecord::Schema.define(version: 2020_03_28_141512) do
     t.boolean "available"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "players_profiles", id: false, force: :cascade do |t|
+    t.integer "player_id"
+    t.integer "profile_id"
+    t.index ["player_id"], name: "index_players_profiles_on_player_id"
+    t.index ["profile_id"], name: "index_players_profiles_on_profile_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "seasons", force: :cascade do |t|
+    t.integer "player_id"
+    t.integer "user_id"
+    t.string "name"
+    t.string "team"
+    t.string "country"
+    t.string "min"
+    t.string "points"
+    t.string "fgp"
+    t.string "three_fgp"
+    t.string "orb"
+    t.string "drb"
+    t.string "trb"
+    t.string "ast"
+    t.string "blk"
+    t.string "stl"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["player_id"], name: "index_seasons_on_player_id"
+    t.index ["user_id"], name: "index_seasons_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
