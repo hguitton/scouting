89ed56c32666b0b4a08eb8f980c4688155b4fae1
Hugo Guitton <guitton.h@gmail.com>
2020-03-29 20:54:31 +0200

    Create basic files and index

--- app/decorators/player_decorator.rb
+++ app/decorators/player_decorator.rb
@@ -1,13 +1,21 @@
 class PlayerDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
+  def name
+    object.lastname.upcase + " " +object.firstname
+  end
 
+  def height
+    str = object.height_eu.to_s + "cm "
+    if(object.height_us)
+      str += "(#{object.height_us})"
+    end
+  end
+
+  def weight
+    str = object.weight_eu.to_s + "kg "
+    if(object.weight_us)
+      str += "(#{object.weight_us} lbs)"
+    end
+  end
 end
