227459ae0ef3e0e172f52678759e7deecf8d69ad
Hugo Guitton <guitton.h@gmail.com>
2020-03-29 12:45:21 +0200

    Players Crud

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,64 @@
+class PlayersController < ApplicationController
+  before_action :set_player, only: [:show, :edit, :update, :destroy]
+
+  # GET /players
+  def index
+    @players = Player.all
+  end
+
+  # GET /players/1
+  def show
+  end
+
+  # GET /players/new
+  def new
+    @player = Player.new
+  end
+
+  # GET /players/1/edit
+  def edit
+  end
+
+  # POST /players
+  def create
+    @player = Player.new(player_params)
+
+    respond_to do |format|
+      if @player.save
+        format.html { redirect_to @player, notice: 'Player was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /players/1
+  def update
+    respond_to do |format|
+      if @player.update(player_params)
+        format.html { redirect_to @player, notice: 'Player was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /players/1
+  def destroy
+    @player.destroy
+    respond_to do |format|
+      format.html { redirect_to players_url, notice: 'Player was successfully destroyed.' }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_player
+      @player = Player.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def player_params
+      params.fetch(:player, {})
+    end
+end
