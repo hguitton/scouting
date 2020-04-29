020b9040908b087e154fd48d1a61887fbbc1c7b1
hguitton <guitton.h@gmail.com>
2020-04-29 17:35:26 +0200

    Changes 29 04 (#34)
    * Fix signup blocked
    
    * Change Available type
    
    * Load agent

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_145954) do
+ActiveRecord::Schema.define(version: 2020_04_29_144757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2020_04_27_145954) do
     t.integer "weight_us"
     t.integer "weight_eu"
     t.string "salary_estimation"
-    t.boolean "available"
+    t.string "available"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "program"
