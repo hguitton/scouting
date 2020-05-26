2016c765168a278affc1bb14adff2a29d22a4c90
Hugo Guitton <guitton.h@gmail.com>
2020-05-26 20:20:02 +0200

    Add roster col

--- app/javascript/packs/controllers/tabulator_controller.js
+++ app/javascript/packs/controllers/tabulator_controller.js
@@ -1,29 +1,23 @@
 import { Controller } from "stimulus";
 import * as moment from "moment";
 window.moment = moment;
-import $ from 'jquery';  
-window.jQuery = $; window.$ = $;
 import Tabulator from "tabulator-tables";
 
 export default class extends Controller {
   connect() {
     //define table
     var table = new Tabulator("#tabulator-players", {
-      layout:"fitColumns",
-      responsiveLayout:"hide",
-      persistence:{
-        sort:true,
-        columns:true,
-      },
+      layout: "fitColumns",
+      responsiveLayout: "hide",
       columns: [
         {
           title: "",
           field: "favorite",
           formatter: this.customFormatter,
-          hozAlign : 'center',
-          vertAlign : 'middle',
+          hozAlign: "center",
+          vertAlign: "middle",
           responsive: 0,
-          width: '40'
+          width: "40",
         },
         {
           title: "Update",
@@ -32,11 +26,12 @@ export default class extends Controller {
           formatterParams: {
             humanize: true,
             invalidPlaceholder: "(invalid date)",
+            suffix: true,
           },
-          hozAlign : 'center',
-          vertAlign : 'middle',
-          width: '80',
-          responsive: 4
+          hozAlign: "center",
+          vertAlign: "middle",
+          width: "80",
+          responsive: 4,
         },
         {
           title: "Name",
@@ -46,54 +41,53 @@ export default class extends Controller {
             labelField: "name",
             target: "_blank",
           },
-          vertAlign : 'middle',
+          vertAlign: "middle",
           width: "10%",
-          minWidth: '100',
-          responsive: 0
+          minWidth: "100",
+          responsive: 0,
         },
         {
           title: "Age",
           field: "birthdate",
           formatter: "datetimediff",
           formatterParams: {
-            inputFormat: "DD-MM-YYYY",
             humanize: true,
             invalidPlaceholder: "(invalid date)",
           },
-          hozAlign : 'center',
-          vertAlign : 'middle',
-          width: '70',
-          responsive: 3
+          hozAlign: "center",
+          vertAlign: "middle",
+          width: "70",
+          responsive: 2,
         },
         {
           title: "Height",
           field: "height",
           formatter: this.customFormatter,
           sorter: this.customSorter,
-          width: '70',
+          width: "70",
         },
         {
           title: "Weight",
           field: "weight",
           formatter: this.customFormatter,
           sorter: this.customSorter,
-          width: '70',
-          responsive: 3
+          width: "70",
+          responsive: 3,
         },
-        { 
-          title: "Status", 
+        {
+          title: "Status",
           field: "status",
-          width: '75',
-          hozAlign : 'center',
-          vertAlign : 'middle'
+          width: "75",
+          hozAlign: "center",
+          vertAlign: "middle",
         },
-        { 
+        {
           title: "Nat.",
           field: "nationality",
-          width: '115',
-          hozAlign : 'center',
-          vertAlign : 'middle',
-          responsive: 3  
+          width: "115",
+          hozAlign: "center",
+          vertAlign: "middle",
+          responsive: 3,
         },
         {
           title: "Pos.",
@@ -101,39 +95,38 @@ export default class extends Controller {
           formatter: this.customFormatter,
           sorter: this.customSorter,
           widthGrow: 1,
-          hozAlign : 'center',
-          vertAlign : 'middle',
-          width: '70'
+          hozAlign: "center",
+          vertAlign: "middle",
+          width: "70",
         },
         {
           title: "Profiles",
           field: "profiles",
           headerSort: false,
           formatter: this.customFormatter,
-          widthGrow: 3,
-          hozAlign : 'center',
-          vertAlign : 'middle',
-          minWidth : "250",
+          hozAlign: "center",
+          vertAlign: "middle",
+          minWidth: "100",
         },
         {
           title: "Salary",
           field: "salary",
           formatter: this.customFormatter,
           sorter: this.customSorter,
-          hozAlign : 'center',
-          vertAlign : 'middle',
-          minWidth: '120',
-          responsive: 1
+          hozAlign: "center",
+          vertAlign: "middle",
+          minWidth: "120",
+          responsive: 1,
         },
         {
           title: "Agent",
           field: "agent",
           headerSort: false,
           formatter: this.customFormatter,
-          hozAlign : 'center',
-          vertAlign : 'middle',
-          width: '90',
-          responsive: 4
+          hozAlign: "center",
+          vertAlign: "middle",
+          width: "90",
+          responsive: 4,
         },
         {
           title: "Last season",
@@ -141,68 +134,89 @@ export default class extends Controller {
           headerSort: false,
           formatter: this.customFormatter,
           responsive: 4,
-          minWidth: '120'
+          minWidth: "150",
         },
         {
           title: "Comments",
           field: "comments",
           formatter: this.customFormatter,
           sorter: this.customSorter,
-          hozAlign : 'center',
-          vertAlign : 'middle',
-          minWidth: '90',
-          responsive: 1
+          hozAlign: "center",
+          vertAlign: "middle",
+          minWidth: "90",
+          responsive: 1,
         },
-        { 
+        {
           title: "Priority",
           field: "priority",
-          hozAlign : 'center',
-          vertAlign : 'middle',
-          minWidth: '80',
-          responsive: 3 
+          hozAlign: "center",
+          vertAlign: "middle",
+          minWidth: "80",
+          responsive: 3,
         },
-        { 
+        {
           title: "Available",
           field: "available",
-          hozAlign : 'center',
-          vertAlign : 'middle',
-          minWidth: '80',
-          responsive: 4
+          hozAlign: "center",
+          vertAlign: "middle",
+          minWidth: "80",
+          responsive: 4,
+        },
+        {
+          title: "Ros.",
+          field: "id",
+          formatter: this.customFormatter,
+          hozAlign: "center",
+          vertAlign: "middle",
+          minWidth: "15",
+          responsive: 0,
         },
       ],
     });
     // define URL data json
     table.setData("players.json");
-    window.addEventListener('resize', function(){
+
+    window.addEventListener("resize", function () {
       table.redraw();
-  });
+    });
   }
 
   customFormatter(cell, formatterParams, onRendered) {
     var valueReturn = "";
     if (cell.getValue() != null) {
+      console.log(cell.getField());
       switch (cell.getField()) {
         case "favorite":
-          if(cell.getValue().value){
-            valueReturn = 
-              `<span class='icon has-text-warning' data-controller='favorite' data-favorite-url='`+ cell.getValue().remove_link +`'>
+          if (cell.getValue().value) {
+            valueReturn =
+              `<span class='icon has-text-warning' data-controller='favorite' data-favorite-url='` +
+              cell.getValue().remove_link +
+              `'>
                 <i class='fas fa-star' data-action='click->favorite#unfav'></i>
-              </span>`
-          }else{
-            valueReturn = 
-              `<span class='icon' data-controller='favorite' data-favorite-url='`+ cell.getValue().add_link +`'>
+              </span>`;
+          } else {
+            valueReturn =
+              `<span class='icon' data-controller='favorite' data-favorite-url='` +
+              cell.getValue().add_link +
+              `'>
                 <i class='far fa-star' data-action='click->favorite#fav'></i>
-              </span>`
+              </span>`;
           }
-        break;
+          break;
         case "height":
           valueReturn =
-            cell.getValue().eu + " cm <br/><small>" + cell.getValue().us + " ft</small>";
+            cell.getValue().eu +
+            " cm <br/><small>" +
+            cell.getValue().us +
+            " ft</small>";
           break;
 
         case "weight":
           valueReturn =
-            cell.getValue().eu + " kg <br/><small>" + cell.getValue().us + " lbs</small>";
+            cell.getValue().eu +
+            " kg <br/><small>" +
+            cell.getValue().us +
+            " lbs</small>";
           break;
 
         case "agent":
@@ -213,15 +227,26 @@ export default class extends Controller {
           valueReturn = cell.getValue().short;
           break;
 
+        case "bob":
+          console.log("COUCOUCOUCOUC");
+          valueReturn = cell.getField();
+          break;
+
         case "salary":
           if (cell.getValue().real != 0) valueReturn = cell.getValue().real;
           else valueReturn = cell.getValue().estimation;
           break;
 
         case "seasons":
-          if(cell.getValue()[0]){
-            var season = cell.getValue()[0]
-            valueReturn = season.name + " <br/> " + season.country + " <br/> <strong>" + season.team + "</strong>"
+          if (cell.getValue()[0]) {
+            var season = cell.getValue()[0];
+            valueReturn =
+              season.name +
+              " <br/> " +
+              season.country +
+              " <br/> <strong>" +
+              season.team +
+              "</strong>";
           }
           break;
 
@@ -242,11 +267,31 @@ export default class extends Controller {
           break;
 
         case "profiles":
+          valueReturn = "<div class='flex fdc'>";
           cell
             .getValue()
-            .forEach((element) => (valueReturn += "<a href='" + element.link + "' class='tag is-info m-xxs'>" + element.name + " </a> "));
+            .forEach(
+              (element) =>
+                (valueReturn +=
+                  "<a href='" +
+                  element.link +
+                  "' class='tag is-info m-xxs'>" +
+                  element.name +
+                  " </a>")
+            );
+          valueReturn += "</div>";
           break;
+        case "id":
+          valueReturn =
+            `
+            <div data-controller="modal">
+              <span class="icon is-medium has-text-success" 
+                data-selector="rosters-modal" 
+                data-action="click->modal#open click->roster-selection#setPlayer" 
+                data-player="` + cell.getValue()+`">
+              <i class="fas fa-lg fa-plus-square"></i></span></div>`;
 
+          break;
         default:
           valueReturn = "error : " + cell.getField();
           break;
--- app/views/players/_player.json.jbuilder
+++ app/views/players/_player.json.jbuilder
@@ -1,33 +1,33 @@
-json.(player, :name, :nationality, :available, :created_at)
+json.(player, :id, :name, :nationality, :available, :created_at)
 
 json.birthdate player.birthdate_timestamp
 
 json.favorite do 
-  json.value current_user.favorite_players.include?(player)
-  json.remove_link remove_favorite_user_path(current_user.id, player.id)
-  json.add_link add_favorite_user_path(current_user.id, player.id)
+json.value current_user.favorite_players.include?(player)
+json.remove_link remove_favorite_user_path(current_user.id, player.id)
+json.add_link add_favorite_user_path(current_user.id, player.id)
 end
 json.updated_at player.updated_at.in_time_zone('Paris')
 json.updated_by_user
 
 json.height do
-    json.eu player.height_eu
-    json.us player.height_us
+  json.eu player.height_eu
+  json.us player.height_us
 end
 
 json.weight do
-    json.eu player.weight_eu
-    json.us player.weight_us
+  json.eu player.weight_eu
+  json.us player.weight_us
 end
 
 json.salary do
-    json.estimation player.salary_estimation
-    json.real player.salary_real
+  json.estimation player.salary_estimation
+  json.real player.salary_real
 end
 
 json.agent do
-    json.eu player.agent_fr
-    json.us player.agent_us
+  json.eu player.agent_fr
+  json.us player.agent_us
 end
 
 json.profiles player.profiles do |profile|
@@ -42,9 +42,9 @@ json.priority player.priority.name
 json.position player.position, :short, :order
 
 json.comments player.comments do |comment|
-    json.content comment.content
-    json.created_at comment.created_at
-    json.created_by comment.created_by.name
+  json.content comment.content
+  json.created_at comment.created_at
+  json.created_by comment.created_by.name
 end
 
 json.seasons player.seasons, :name, :country, :team, :min, :points, :fgp, :three_fgp, :orb, :drb, :trb, :ast, :blk, :stl
